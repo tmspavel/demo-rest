a6d1be91bda002f6b256a2033c13269aee641562
Pavel Iliukevich <pavel.iliukevich@resliv.by>
2023-06-08 00:09:35 +0300

    security simple example

--- src/main/java/com/tms/demorest/config/SecurityOldConfig.java
+++ src/main/java/com/tms/demorest/config/SecurityOldConfig.java
@@ -1,29 +1,33 @@
-//package com.tms.demorest.config;
-//
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-////@Configuration
-//public class SecurityOldConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests()
-//            .antMatchers("/loginpage").permitAll()
-//            .antMatchers("/mainpage").authenticated()
-//            .and()
-//            .httpBasic();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        String user = "user";
-//        String admin = "admin";
-//        auth.inMemoryAuthentication()
-////            .userDetailsPasswordManager(NoOpPasswordEncoder.getInstance())
-//            .withUser(user).password(user).authorities("read")
-//            .and()
-//            .withUser(admin).password(admin).authorities("write");
-//    }
-//}
+package com.tms.demorest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityOldConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+            .antMatchers("/mainpage", "/").permitAll()
+            .antMatchers("/mypage").authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String user = "user";
+        String admin = "admin";
+        auth.inMemoryAuthentication()
+            .passwordEncoder(NoOpPasswordEncoder.getInstance())
+            .withUser(user).password(user).authorities("read")
+            .and()
+            .withUser(admin).password(admin).authorities("write");
+    }
+}
--- src/main/java/com/tms/demorest/controller/MainController.java
+++ src/main/java/com/tms/demorest/controller/MainController.java
@@ -1,15 +1,20 @@
 package com.tms.demorest.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class MainController {
 
-    @GetMapping("/main")
+    @GetMapping("/mainpage")
     public String main() {
-        return "mainPage.html";
+        return "main-page.html";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage() {
+        return "my-page.html";
     }
 }
